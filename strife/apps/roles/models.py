9f9c0aa45cce8385adeaf3b233e50b314b08f9c9
Krishnan Shankar <krishnans2006@gmail.com>
2024-07-31 22:52:05 -0400

    Improve model fields

--- strife/apps/roles/models.py
+++ strife/apps/roles/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from apps.servers.models import Server
+
 
 class Role(models.Model):
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=256)
-
     color = models.CharField(max_length=6)  # hex
+
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- strife/apps/servers/models.py
+++ strife/apps/servers/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from apps.roles.models import Role
 from apps.users.models import User
 
 
 class Server(models.Model):
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=256)
+
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
@@ -17,6 +20,9 @@ class Server(models.Model):
 class Member(User):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     server = models.ForeignKey(Server, on_delete=models.CASCADE)
+
+    nickname = models.CharField(max_length=32, blank=True)
+    roles = models.ManyToManyField(Role, blank=True)
 
     joined_at = models.DateTimeField(auto_now_add=True)
 
