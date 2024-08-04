d808a1c1da1274d2ffbb34dcaf45336c50f7614b
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-04 17:06:18 -0400

    Add login detection to home page

--- strife/apps/users/urls.py
+++ strife/apps/users/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("register/", views.RegisterView.as_view(), name="register"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
