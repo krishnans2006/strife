b1ba1dcf07fe90f25b4e1086dcfc7448f9386c20
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-05 15:52:55 -0400

    Simplify channels admin

--- strife/apps/channels/admin.py
+++ strife/apps/channels/admin.py
@@ -4,6 +4,8 @@ from .models import Messageable, Speakable, TextChannel, ForumChannel, VoiceChan
 
 
 @admin.register(Messageable)
+@admin.register(TextChannel)
+@admin.register(ForumChannel)
 class MessageableAdmin(admin.ModelAdmin):
     list_display = ("name", "server")
     ordering = ("created_at",)
@@ -13,43 +15,9 @@ class MessageableAdmin(admin.ModelAdmin):
 
 
 @admin.register(Speakable)
-class SpeakableAdmin(admin.ModelAdmin):
-    list_display = ("name", "server")
-    ordering = ("created_at",)
-    search_fields = ("name",)
-    autocomplete_fields = ("server",)
-    save_as = True
-
-
-@admin.register(TextChannel)
-class TextChannelAdmin(admin.ModelAdmin):
-    list_display = ("name", "server")
-    ordering = ("created_at",)
-    search_fields = ("name",)
-    autocomplete_fields = ("server",)
-    save_as = True
-
-
-@admin.register(ForumChannel)
-class ForumChannelAdmin(admin.ModelAdmin):
-    list_display = ("name", "server")
-    ordering = ("created_at",)
-    search_fields = ("name",)
-    autocomplete_fields = ("server",)
-    save_as = True
-
-
 @admin.register(VoiceChannel)
-class VoiceChannelAdmin(admin.ModelAdmin):
-    list_display = ("name", "server")
-    ordering = ("created_at",)
-    search_fields = ("name",)
-    autocomplete_fields = ("server",)
-    save_as = True
-
-
 @admin.register(StageChannel)
-class StageChannelAdmin(admin.ModelAdmin):
+class SpeakableAdmin(admin.ModelAdmin):
     list_display = ("name", "server")
     ordering = ("created_at",)
     search_fields = ("name",)
