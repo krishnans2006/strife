80c7259a70d447a72bc573de16db435365841dab
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-05 15:27:53 -0400

    Register channel types with Django Admin

--- strife/apps/channels/admin.py
+++ strife/apps/channels/admin.py
@@ -1,10 +1,55 @@
 from django.contrib import admin
 
-from .models import Messageable
+from .models import Messageable, Speakable, TextChannel, ForumChannel, VoiceChannel, StageChannel
 
 
 @admin.register(Messageable)
 class MessageableAdmin(admin.ModelAdmin):
+    list_display = ("name", "server")
+    ordering = ("created_at",)
+    search_fields = ("name",)
+    autocomplete_fields = ("server",)
+    save_as = True
+
+
+@admin.register(Speakable)
+class SpeakableAdmin(admin.ModelAdmin):
+    list_display = ("name", "server")
+    ordering = ("created_at",)
+    search_fields = ("name",)
+    autocomplete_fields = ("server",)
+    save_as = True
+
+
+@admin.register(TextChannel)
+class TextChannelAdmin(admin.ModelAdmin):
+    list_display = ("name", "server")
+    ordering = ("created_at",)
+    search_fields = ("name",)
+    autocomplete_fields = ("server",)
+    save_as = True
+
+
+@admin.register(ForumChannel)
+class ForumChannelAdmin(admin.ModelAdmin):
+    list_display = ("name", "server")
+    ordering = ("created_at",)
+    search_fields = ("name",)
+    autocomplete_fields = ("server",)
+    save_as = True
+
+
+@admin.register(VoiceChannel)
+class VoiceChannelAdmin(admin.ModelAdmin):
+    list_display = ("name", "server")
+    ordering = ("created_at",)
+    search_fields = ("name",)
+    autocomplete_fields = ("server",)
+    save_as = True
+
+
+@admin.register(StageChannel)
+class StageChannelAdmin(admin.ModelAdmin):
     list_display = ("name", "server")
     ordering = ("created_at",)
     search_fields = ("name",)
