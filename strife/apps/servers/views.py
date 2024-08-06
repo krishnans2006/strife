1f49ee97940ccb56934020e3751e78a31e270a69
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-05 22:15:28 -0400

    Fix server creation view

--- strife/apps/servers/views.py
+++ strife/apps/servers/views.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from .models import Server
+from .models import Server, Member
 
 
-class ServerCreateView(CreateView):
+class ServerCreateView(CreateView, LoginRequiredMixin):
     model = Server
     fields = ("name", "description", "image")
     template_name = "forms/create.html"
@@ -15,3 +16,7 @@ class ServerCreateView(CreateView):
         "description": "Create a new server.",
         "form_button_text": "Create Server"
     }
+
+    def form_valid(self, form):
+        form.instance.owner = Member.objects.create(user=self.request.user, server=form.instance)
+        return super().form_valid(form)
