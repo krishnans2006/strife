21642f6216436286e0551fc5139e9df5d285a26f
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-07 21:08:28 -0400

    Fix PolymorphicModel related_name and add migrations

--- strife/apps/channels/models.py
+++ strife/apps/channels/models.py
@@ -8,7 +8,8 @@ class Channel(PolymorphicModel):
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=256)
 
-    server = models.ForeignKey(Server, on_delete=models.CASCADE)  # Do not add related_name
+    # %(class)ss => channels, messageables, textchannels, etc.
+    server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="%(class)ss")
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -21,34 +22,27 @@ class Channel(PolymorphicModel):
 
     class Meta:
         abstract = True
-        default_related_name = "channels"
 
 
 class Messageable(Channel):
-    class Meta:
-        default_related_name = "messageable_channels"
+    pass
 
 
 class TextChannel(Messageable):
-    class Meta:
-        default_related_name = "text_channels"
+    pass
 
 
 class ForumChannel(Messageable):
-    class Meta:
-        default_related_name = "forum_channels"
+    pass
 
 
 class Speakable(Channel):
-    class Meta:
-        default_related_name = "speakable_channels"
+    pass
 
 
 class VoiceChannel(Speakable):
-    class Meta:
-        default_related_name = "voice_channels"
+    pass
 
 
 class StageChannel(Speakable):
-    class Meta:
-        default_related_name = "stage_channels"
+    pass
