ba6b2cd8aa12cea3401b5a7c2026aa847688b0f0
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-09 22:17:20 -0400

    Implement a group-based message consumer system

--- strife/apps/messages/consumers.py
+++ strife/apps/messages/consumers.py
@@ -1,5 +1,6 @@
 import json
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 from .models import Message
@@ -7,10 +8,15 @@ from .models import Message
 
 class MessageConsumer(WebsocketConsumer):
     def connect(self):
+        self.channel_id = self.scope["url_route"]["kwargs"]["channel_id"]
+        self.room_group_name = f"chat_{self.channel_id}"
+
+        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -18,6 +24,15 @@ class MessageConsumer(WebsocketConsumer):
 
         Message.objects.create(content=message)
 
-        self.send(text_data=json.dumps({
-            "message": message
-        }))
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type": "chat.message",
+                "message": message,
+            },
+        )
+
+    def chat_message(self, event):
+        message = event["message"]
+
+        self.send(text_data=json.dumps({"message": message}))
