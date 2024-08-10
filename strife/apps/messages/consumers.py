55ae04bafad7469514f9b1fd02bc7004863220c1
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-09 23:06:43 -0400

    Implement websockets in message view

--- strife/apps/messages/consumers.py
+++ strife/apps/messages/consumers.py
@@ -9,23 +9,29 @@ from .models import Message
 class MessageConsumer(WebsocketConsumer):
     def connect(self):
         self.channel_id = self.scope["url_route"]["kwargs"]["channel_id"]
-        self.room_group_name = f"chat_{self.channel_id}"
+        self.user = self.scope["user"]
 
-        async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
+        self.channel_group_name = f"chat_{self.channel_id}"
+
+        async_to_sync(self.channel_layer.group_add)(self.channel_group_name, self.channel_name)
 
         self.accept()
 
     def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
+        async_to_sync(self.channel_layer.group_discard)(self.channel_group_name, self.channel_name)
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        content = text_data_json["content"]
 
-        Message.objects.create(content=message)
+        message = Message.objects.create(
+            author=self.user,
+            channel_id=self.channel_id,
+            content=content
+        )
 
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
+            self.channel_group_name,
             {
                 "type": "chat.message",
                 "message": message,
