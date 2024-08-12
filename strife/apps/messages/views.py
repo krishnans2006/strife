0aaecdb9b5592a3f585eb0cc195c161e55dbb203
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-12 15:56:05 -0400

    Switch to FileResponses

--- strife/apps/messages/views.py
+++ strife/apps/messages/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import HttpResponseNotAllowed, JsonResponse, HttpResponse
+from django.http import HttpResponseNotAllowed, JsonResponse, FileResponse
 
 from .models import Message, Attachment
 
@@ -26,14 +26,14 @@ def send_message_view(request, server_id, channel_id):
 def view_attachment_view(request, server_id, channel_id, message_id, attachment_id):
     attachment = Attachment.objects.get(id=attachment_id)
 
-    response = HttpResponse(attachment.file, content_type="unknown")
-    response["Content-Disposition"] = f"inline; filename={attachment.filename}"
-    return response
+    file_obj = open(attachment.file.path, "rb")
+
+    return FileResponse(file_obj, filename=attachment.filename, as_attachment=False)
 
 
 def download_attachment_view(request, server_id, channel_id, message_id, attachment_id):
     attachment = Attachment.objects.get(id=attachment_id)
 
-    response = HttpResponse(attachment.file, content_type="application/force-download")
-    response["Content-Disposition"] = f"attachment; filename={attachment.filename}"
-    return response
+    file_obj = open(attachment.file.path, "rb")
+
+    return FileResponse(file_obj, filename=attachment.filename, as_attachment=True)
