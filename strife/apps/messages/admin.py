c3bd7d8921e0bf09886ce075fe3090a8a685e20c
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-12 01:19:50 -0400

    Register attachments with Django Admin

--- strife/apps/messages/admin.py
+++ strife/apps/messages/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Message
+from .models import Message, Attachment
 
 
 @admin.register(Message)
@@ -9,4 +9,13 @@ class MessageAdmin(admin.ModelAdmin):
     ordering = ("created_at",)
     search_fields = ("content",)
     autocomplete_fields = ("author", "channel")
+    save_as = True
+
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "message", "file")
+    ordering = ("message__created_at",)
+    search_fields = ("message__content", "file")
+    autocomplete_fields = ("message",)
     save_as = True
