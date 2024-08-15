62ea7f4321c49c0b5cf7bbb770563cd27f05af76
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-15 02:34:24 -0400

    Add more role functions as views/urls

--- strife/apps/roles/urls.py
+++ strife/apps/roles/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path("create/", views.RoleCreateView.as_view(), name="create"),
+    path("list/", views.RoleListView.as_view(), name="list"),
     path("<int:role_id>/edit/", views.RoleEditView.as_view(), name="edit"),
+    path("<int:role_id>/delete/", views.RoleDeleteView.as_view(), name="delete"),
 ]
--- strife/apps/roles/views.py
+++ strife/apps/roles/views.py
@@ -2,10 +2,28 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, ListView, DeleteView
 
 from .models import Role
 from ..servers.models import Server
+
+
+class RoleListView(LoginRequiredMixin, ListView):
+    model = Role
+    template_name = "roles/list.html"
+    context_object_name = "roles"
+    paginate_by = 10
+
+    def get_queryset(self):
+        server_id = self.kwargs.get("server_id")
+        server = Server.objects.get(id=server_id)
+
+        return Role.objects.filter(server=server)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["server"] = Server.objects.get(id=self.kwargs.get("server_id"))
+        return context
 
 
 class RoleCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
@@ -20,6 +38,19 @@ class RoleCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
         "form_button_text": "Create Role"
     }
 
+    def form_valid(self, form):
+        role = form.save(commit=False)
+
+        server_id = self.kwargs.get("server_id")
+        server = Server.objects.get(id=server_id)
+
+        role.server = server
+        role.save()
+
+        self.object = role
+
+        return HttpResponseRedirect(self.get_success_url())
+
 
 class RoleEditView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = Role
@@ -32,4 +63,17 @@ class RoleEditView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
         "title": "Edit Role",
         "description": "Edit your role.",
         "form_button_text": "Save Changes"
+    }
+
+
+class RoleDeleteView(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
+    model = Role
+    template_name = "forms/page.html"
+    pk_url_kwarg = "role_id"
+    success_url = reverse_lazy("home:dashboard")
+    success_message = "Role deleted successfully."
+    extra_context = {
+        "title": "Delete Role",
+        "description": "Are you sure you want to delete this role?",
+        "form_button_text": "Delete Role"
     }
