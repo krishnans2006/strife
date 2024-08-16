edeeb7c224b0bdac7b81228f7235ae2fbb8ecdd3
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-16 14:21:23 -0400

    Add permission object attributes and validation

--- strife/apps/permissions/models.py
+++ strife/apps/permissions/models.py
@@ -11,7 +11,35 @@ class Permissions(models.Model):
     can_send_attachments = models.BooleanField(default=False)
 
     def __str__(self):
-        return "Permissions object"
+        if self.is_for_server:
+            return f"Permissions for server {self.server.name}"
+        if self.is_for_role:
+            return f"Permissions for role {self.role.name}"
+        if self.is_for_member:
+            return f"Permissions for member {self.member.username}"
+        return "Unassigned permissions object"
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        if sum((self.is_for_server, self.is_for_role, self.is_for_member)) != 1:
+            raise ValueError("Permissions object must be assigned to a server, role, or member")
+        super().clean()
+
+    # Permissions objects can be assigned to either a server, role, or member
+    @property
+    def is_for_server(self):
+        return hasattr(self, "server")
+
+    @property
+    def is_for_role(self):
+        return hasattr(self, "role")
+
+    @property
+    def is_for_member(self):
+        return hasattr(self, "member")
 
     def __repr__(self):
         return "<Permissions object>"
