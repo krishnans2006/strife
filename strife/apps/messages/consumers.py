9b92b101b95083fd80a014bd169858ecc824df36
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-17 17:52:23 -0400

    Use GenericConsumer for message websocket

--- strife/apps/messages/consumers.py
+++ strife/apps/messages/consumers.py
@@ -1,21 +1,19 @@
 import io
 import json
 
+from apps.home.consumers import GenericConsumer
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
 from django.core.files import File
 
 from strife.apps.channels.models import Channel
 from strife.apps.messages.models import Message
 
 
-class MessageConsumer(WebsocketConsumer):
+class MessageConsumer(GenericConsumer):
     BYTES_SEPARATOR = 33  # ! (exclamation mark)
 
-    # Connection
-    def connect(self):
-        self.user = self.scope["user"]
-
+    def initialize(self):
+        # Set parameters
         self.server_id = self.scope["url_route"]["kwargs"]["server_id"]
         self.channel_id = self.scope["url_route"]["kwargs"]["channel_id"]
         assert self.server_id
@@ -25,46 +23,12 @@ class MessageConsumer(WebsocketConsumer):
         self.server = self.channel.server
         assert self.server.id == self.server_id
 
-        self.channel_group_name = f"chat_{self.channel_id}"
+        # Register supported types
+        self.supported_types_json["message"] = self.handle_message_payload
+        self.supported_types_bytes[b"file"] = self.handle_file_payload
 
-        async_to_sync(self.channel_layer.group_add)(self.channel_group_name, self.channel_name)
-
-        self.accept()
-
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(self.channel_group_name, self.channel_name)
-
-    # Receiving
-    def receive(self, text_data=None, bytes_data=None):
-        if text_data:
-            # Is the data valid?
-            text_data_json = json.loads(text_data)
-            type = text_data_json["type"]
-
-            supported_types = {"message"}
-            if type not in supported_types:
-                print("Unsupported type")
-                return
-
-            # Dispatch the payload
-            if type == "message":
-                self.handle_message_payload(text_data_json)
-        else:
-            # Is the data valid?
-            if bytes_data[0] != self.BYTES_SEPARATOR:
-                print("Invalid bytes data")
-                return
-
-            data_chunks = bytes_data[1:].split(b"!")
-
-            supported_commands = {b"file"}
-            if data_chunks[0] not in supported_commands:
-                print("Unsupported command")
-                return
-
-            # Dispatch the payload
-            if data_chunks[0] == b"file":
-                self.handle_file_payload(data_chunks)
+        # Set group name
+        self.group_name = f"chat_{self.channel_id}"
 
     def handle_message_payload(self, payload):
         # Are they allowed to send messages?
@@ -81,7 +45,7 @@ class MessageConsumer(WebsocketConsumer):
 
         # Update websocket clients
         async_to_sync(self.channel_layer.group_send)(
-            self.channel_group_name,
+            self.group_name,
             {
                 "type": "chat.message",
                 "message": message.to_dict(),
@@ -108,14 +72,13 @@ class MessageConsumer(WebsocketConsumer):
 
         # Update websocket clients
         async_to_sync(self.channel_layer.group_send)(
-            self.channel_group_name,
+            self.group_name,
             {
                 "type": "chat.attachment",
                 "message": message.to_dict(),
             },
         )
 
-    # Sending
     def chat_message(self, event):
         message = event["message"]
 
