a68c056c26895688e8a6c49652d33e1db2798f84
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-19 16:12:57 -0400

    Add ordering for role queries

--- strife/apps/roles/models.py
+++ strife/apps/roles/models.py
@@ -19,6 +19,9 @@ class Role(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ("name",)
+
     def __str__(self):
         return self.name
 
