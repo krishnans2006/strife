c2c7254f069748b2e990a8c4555b7a6c6960595e
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-19 18:26:12 -0400

    Change JS logic to use HTTP for attachment uploads

--- strife/apps/messages/urls.py
+++ strife/apps/messages/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Must be changed in static/js/websockets/send-handlers/messages.js
+    path(
+        "<int:message_id>/attachments/upload/",
+        views.upload_attachment_view,
+        name="upload_attachment",
+    ),
     path(
         "<int:message_id>/attachments/<int:attachment_id>/view/",
         views.view_attachment_view,
--- strife/static/js/websockets/send-handlers/messages.js
+++ strife/static/js/websockets/send-handlers/messages.js
@@ -1,45 +1,22 @@
 // Messages-specific socket functions
 
-function sendFile(fileObj, fileData, messageID) {
-    const fileMetadata = JSON.stringify({
-        name: fileObj.name,
-        type: fileObj.type,
-        size: fileObj.size,
-        messageID: messageID,
-    });
-
-    const enc = new TextEncoder();
-    const prefix = enc.encode('!file!');
-    const metadata = enc.encode(fileMetadata);
-    const separator = enc.encode('!');
-    const data = fileData;
-
-    let toSend = new Uint8Array(prefix.byteLength + metadata.byteLength + separator.byteLength + data.byteLength);
-    toSend.set(new Uint8Array(prefix), 0);
-    toSend.set(new Uint8Array(metadata), prefix.byteLength);
-    toSend.set(new Uint8Array(separator), prefix.byteLength + metadata.byteLength);
-    toSend.set(new Uint8Array(data), prefix.byteLength + metadata.byteLength + separator.byteLength);
-
-    socket.binaryType = 'arraybuffer';
-    socket.send(toSend);
-    socket.binaryType = 'blob';
-}
-
-function sendSocketAttachments() {
+function sendAttachments(message_id) {
     if (uploadedFiles.length > 0) {
         // Use FileReader and ArrayBuffer to send files
         for (let i = 0; i < uploadedFiles.length; i++) {
             const file = uploadedFiles[i];
-            const reader = new FileReader();
 
-            reader.onload = function (e) {
-                let rawData = new ArrayBuffer();
-                rawData = e.target.result;
+            let formData = new FormData();
+            formData.append('file', file);
 
-                sendFile(file, rawData, lastMessage.id);
-            };
-
-            reader.readAsArrayBuffer(file);
+            // POST to be processed by Django
+            $.ajax({
+                url: `/servers/${SERVER_ID}/channels/${CHANNEL_ID}/messages/${message_id}/attachments/upload/`,
+                type: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
+            });
         }
 
         uploadedFiles = [];
@@ -67,7 +44,7 @@ function sendSocketMessage() {
                 setTimeout(waitForMessageUpdateThenUploadAttachments, 100);
                 return;
             }
-            sendSocketAttachments();
+            sendAttachments(lastMessage.id);
         }
 
         waitForMessageUpdateThenUploadAttachments();
