664cac584730440c104d6d3746596f34312667c1
Krishnan Shankar <krishnans2006@gmail.com>
2024-08-19 20:36:22 -0400

    Standardize message detail component

--- strife/static/js/websockets/recv-handlers/messages.js
+++ strife/static/js/websockets/recv-handlers/messages.js
@@ -5,22 +5,27 @@ function messageHandler(message) {
 
     // Any updates here should also be done to messages/detail.html
     messageList.innerHTML += `
-          <div id="message-${message.id}" class="w-full px-5 py-1 justify-center flex flex-row gap-3.5 relative">
-            <a id="message-${message.ID}-user-icon" class="cursor-pointer user-icon-popup-link">
-              <img class="size-10 flex-none aspect-square object-cover rounded-full mt-1"
-                   src="${message.author.display_avatar}" alt="${message.author.username}">
-              <div class="hidden user-icon-popup" id="message-${message.id}-profile">
-                {% include 'users/profile.html' with user=message.author %}
-              </div>
+          <div id="message-${message.id}" class="w-full px-5 py-1 justify-center flex flex-row gap-3.5 flex-none">
+            <a>
+              <img id="message-${message.ID}-user-icon"
+                   class="cursor-pointer size-10 flex-none aspect-square object-cover rounded-full mt-1 user-icon-popup-link"
+                   src="${message.serverized_author.display_avatar}"
+                   alt="${message.serverized_author.username}"
+                   data-user-id="${message.serverized_author.id}"
+                   onclick="sendMemberRequest(${message.serverized_author.id})">
             </a>
-            <div id="message-${message.id}-child" class="grow text-gray-300 flex flex-col">
-              <h4 class="text-lg font-bold">${message.author.username}</h4>
+            <div id="message-${message.id}-child"
+                 class="grow text-gray-300 flex flex-col">
+              <h4 class="text-lg font-bold">${message.serverized_author.username}</h4>
               <p>${message.content}</p>
 
               ${message.attachments.length > 0 ? `
-                <div id="message-${message.id}-attachments" class="flex flex-row gap-3.5">
+                <div id="message-${message.id}-attachments"
+                     class="flex flex-row gap-3.5">
                   ${message.attachments.map((attachment) => `
-                    <a href="${attachment.download_url}" target="_blank" class="inline-block min-w-20 py-1 px-2.5 rounded-xl bg-gray-700 text-white cursor-pointer">${attachment.name}</a>
+                    <a href="${attachment.download_url}"
+                       target="_blank"
+                       class="inline-block py-1 px-2.5 rounded-xl bg-gray-700 text-white cursor-pointer">${attachment.name}</a>
                   `).join('')}
                 </div>
               ` : ''}
